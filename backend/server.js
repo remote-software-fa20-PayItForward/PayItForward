3842fb7cf337b9a9d767ba7b71c6bf4e0f067577
Allen Lin <al5361@nyu.edu>
2020-10-31 15:18:40 -0400

    Added Plaid API Keys

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const plaid = require('plaid');
+
+//API keys are stored in keys.js
+const {PLAID_CLIENT_ID, PLAID_SECRET} = require('./keys');
+
+//initialize Plaid API, need post/get functions to display page
+const client = new plaid.Client({
+    clientID: PLAID_CLIENT_ID,
+    secret: PLAID_SECRET,
+    env: plaid.environments.sandbox
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
