f6dc65598235524d81d5ee18ac3161722c6d9369
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2020-11-02 01:17:23 -0500

    homepage should be at /, not /home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,8 @@ import Login from './Login';
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Login} />
-      <Route exact path="/home" component={HomePage} />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
     </Router>
   );
