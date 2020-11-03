ed53031a782a6dbb69d5d4c1b74f09607cae5642
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2020-11-03 12:18:35 -0500

    new frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import Register from './Register';
 import Login from './Login';
+import MFA from './MFA';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route exact path="/mfa" component={MFA} />
     </Router>
   );
 }
