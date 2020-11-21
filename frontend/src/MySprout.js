df205c42bcff58233e4017ce9a60e56bee56b0fe
Matthew Stillman <mstillman9@verizon.net>
2020-11-21 16:23:31 -0500

    use react bootstrap

--- frontend/src/MySprout.js
+++ frontend/src/MySprout.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import React, { Component } from "react";
 import NavBar from './Navbar';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Badge from 'react-bootstrap/Badge';
+
 
 class MySprout extends Component{
 	
@@ -47,57 +53,57 @@ class MySprout extends Component{
 				
 				<h1 className="purple-text text-center font-weight-bold mt-5" >My Sprout</h1>
 				
-				<div className="row justify-content-center">
-					<div className="card my-5  shadow-lg purple-bg" style={{width: '50%'}}>
+				<Row className="justify-content-center">
+					<Card className="my-5  shadow-lg purple-bg" style={{width: '50%'}}>
 						<div className="border mt-5 bg-white" >
-						<img className="card-img-top " src={this.state.image} alt="Card image cap" style={{width: '50%', marginLeft: '25%'}}/>
+							<Card.Img variant="top" src={this.state.image} alt="Card image cap" style={{width: '50%', marginLeft: '25%'}}/>
 						</div>
 						
-						<div className="col-md-12">
+						<Col md={12}>
 							<h3 className="my-4 text-center">{this.state.name}</h3>
-							<div className="card-group pl-3 pb-3 text-center">
-								<div className="card mr-3 text-dark rounded text-left">
-									<div className="card-body">
+							<CardGroup className="pl-3 pb-3 text-center">
+								<Card className="mr-3 text-dark rounded text-left">
+									<Card.Body>
 										<p className="lead font-weight-bold purple-text">Sprout Description: </p>
 										<p className=""> {this.state.description} </p>
-										<h4><span class="badge badge-primary">#{this.state.category}</span></h4> 
-									</div>
-								</div>
-							</div>
-						</div>
+										<h4><Badge variant="primary">#{this.state.category}</Badge></h4> 
+									</Card.Body>
+								</Card>
+							</CardGroup>
+						</Col>
 						  
-						 <div className="col-md-12">
-							<div className="card-group pl-3 pb-3 text-center">
-								<div className="card mr-3 text-dark rounded">
-									<div className="card-body">
+						<Col md={12}>
+							<CardGroup className="pl-3 pb-3 text-center">
+								<Card className="mr-3 text-dark rounded">
+									<Card.Body>
 										<p className="lead font-weight-bold display-4 purple-text"> ${this.state.amount} </p>
 										<p className="font-weight-bold"> requested sprout amount </p>
-									</div>
-								</div>
+									</Card.Body>
+								</Card>
 								
-								<div className="card mr-3 text-dark rounded">
-									<div className="card-body">
+								<Card className="mr-3 text-dark rounded">
+									<Card.Body>
 										<p className="lead font-weight-bold display-4 purple-text"> 8 </p>
 										<p className="font-weight-bold"> planters growing your sprout </p>
-									</div>
-								</div>
+									</Card.Body>
+								</Card>
 								
-								<div className="card mr-3 text-dark rounded">
-									<div className="card-body ">
+								<Card className="mr-3 text-dark rounded">
+									<Card.Body>
 										<p className="lead font-weight-bold display-4 purple-text"> $35 </p>
 										<p className="font-weight-bold"> current sprout amount </p>
-									</div>
-								</div>
-							</div>
+									</Card.Body>
+								</Card>
+							</CardGroup>
 													
 							<div className="text-center">
-								<Link to="/my-sprout"><Button className="font-weight-bold px-3 mb-3 btn-warning"><h6>Edit My Sprout</h6></Button></Link>
+								<Link to="/my-sprout"><Button className="font-weight-bold px-3 mb-3" variant="warning"><h6>Edit My Sprout</h6></Button></Link>
 								<br />
 							</div>
 							
-						</div>
-					</div>
-				</div>
+						</Col>
+					</Card>
+				</Row>
 				
 			</div>
 			
