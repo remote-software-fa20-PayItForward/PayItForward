672e31e4b74dc49b2657a5e3265ab67a2fe8783e
Matthew Stillman <mstillman9@verizon.net>
2020-11-21 16:48:31 -0500

    use react bootstrap

--- frontend/src/DonationRequest.js
+++ frontend/src/DonationRequest.js
@@ -2,6 +2,10 @@ import './DonationRequest.css';
 import { Link } from 'react-router-dom';
 import React, { Component } from "react";
 import NavBar from './Navbar' 
+import Container from 'react-bootstrap/Container';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form'
 
 class DonationRequest extends Component{
 
@@ -72,61 +76,61 @@ class DonationRequest extends Component{
             <div>
                 <NavBar />
 				
-				<div className="container donation-form shadow-lg">
+				<Container className="donation-form shadow-lg">
 					<div className="donation-image purple-bg">
 						<img src="plant.png" alt="rocket_contact" />
 						<h1 className="mt-3">Create Donation Request</h1>
 					</div>
 					
-					<form onSubmit={(e) => { this.submit(); e.preventDefault(); }} className="bg-light rounded border">
-						<div className="card-group">
-							<div className="col-md-6 border-right">
-								<div className="form-group">
-									<label className="form-control-label font-weight-bold lead">Sprout Name</label>
-									<input type="text" name="name" className="form-control " placeholder="Enter sprout name" onChange={this.handleChange} required />
-								</div>
+					<Form onSubmit={(e) => { this.submit(); e.preventDefault(); }} className="bg-light rounded border">
+						<CardGroup>
+							<Col md={6} className="border-right">
+								<Form.Group>
+									<Form.Label className="font-weight-bold lead" for="name">Sprout Name</Form.Label>
+									<Form.Control type="text" name="name" placeholder="Enter sprout name" onChange={this.handleChange} required />
+								</Form.Group>
 			  
-								<div className="form-group">
-									<label className="form-control-label font-weight-bold lead">Sprout Category</label>
+								<Form.Group>
+									<Form.Label className="font-weight-bold lead" for="category">Sprout Category</Form.Label>
 									<br />
-									<select className="form-control" name="category" onChange={this.handleChange}>
+									<Form.Control as="select" name="category" onChange={this.handleChange}>
 										<option selected value="books">Books</option>
 										<option value="gas">Gas</option>
 										<option value="groceries">Groceries</option>
-									</select>
-								</div>
+									</Form.Control>
+								</Form.Group>
 			  
-								<div className="form-group">
-									<label className="form-control-label font-weight-bold lead">Sprout Amount <span className="small">(max: $50)</span></label>
-									<input type="number" name="amount" className="form-control " placeholder="Enter amount needed" onChange={this.handleChange} defaultValue="0" min="1" max="50" type="number" />
-								</div>
-							</div>
+								<Form.Group>
+									<Form.Label className="font-weight-bold lead" for="amount">Sprout Amount <span className="small">(max: $50)</span></Form.Label>
+									<Form.Control type="number" name="amount" placeholder="Enter amount needed" onChange={this.handleChange} defaultValue="0" min="1" max="50" type="number" />
+								</Form.Group>
+							</Col>
 							
-							<div className="col-md-6">
-								<div className="form-group">
-									<label className="form-control-label font-weight-bold lead">Upload Sprout Image</label>
+							<Col md={6}>
+								<Form.Group>
+									<Form.Label className="font-weight-bold lead" for="image">Upload Sprout Image</Form.Label>
 									<input type="file"  name="image" accept="image/*" onChange={this.handleUpload}/>
-								</div>
+								</Form.Group>
 			  
-								<div className="form-group">
-									<label className="form-control-label font-weight-bold lead">Sprout Description <span className="small">(max: 200 characters)</span></label>
-									<textarea name="description" className="form-control" placeholder="Enter sprout description" maxLength="200" style={{width: '100%', height: '150px'}} onChange={this.handleChange} required />
-								</div>
-							</div>
+								<Form.Group>
+									<Form.Label className="font-weight-bold lead" for="description">Sprout Description <span className="small">(max: 200 characters)</span></Form.Label>
+									<Form.Control as="textarea" name="description" className="form-control" placeholder="Enter sprout description" maxLength="200" style={{width: '100%', height: '150px'}} onChange={this.handleChange} required />
+								</Form.Group>
+							</Col>
  
-						</div>
+						</CardGroup>
 						
-						<div className="card-group mt-3" style={{marginLeft: "30%", width: "80%"}}>
-							<div className="col-md-12">
-								<div className="form-group">
+						<CardGroup className="mt-3" style={{marginLeft: "30%", width: "80%"}}>
+							<Col md={12}>
+								<Form.Group>
 									<input type="submit" name="btnSubmit" className="btnContact" value="Plant your Sprout" />
-								</div>
-							</div>
-						</div>
-					</form>
+								</Form.Group>
+							</Col>
+						</CardGroup>
+					</Form>
 			
+				</Container>
 			</div>
-		</div>
         );
     }
 }
