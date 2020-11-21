a487c611a4b4c7c2b09bf2c238aa733b8dc50069
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2020-11-21 03:38:10 -0500

    whoops

--- backend/routes/donation-request.js
+++ backend/routes/donation-request.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 const upload = multer();
-const Image = require( './models/Image' );
+const Image = require( '../models/Image' );
 const DonationRequest = require('./models/DonationRequest');
 
 
