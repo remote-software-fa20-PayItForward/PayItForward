0a2fde11291df4f98563b2c9a09ed25d23d6aeec
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2020-11-22 13:57:19 -0500

    add routes to backend

--- backend/routes/donation-request.js
+++ backend/routes/donation-request.js
@@ -51,13 +51,25 @@ router.get('/', (req, res, next) => {
     }
 });
 
+router.get('/all', (req, res) => {
+    DonationRequest.find().then((donationRequests) => {
+        if (donationRequests) {
+            return res.json(donationRequests);
+        }
+    }).catch(() => {
+        return res.status(500).json({error: "Cannot get donation requests"});
+    });
+})
+
 router.get('/:id', (req, res, next) => {
-    DonationRequest.findById(req.params.id).then(donationRequest => {
+    DonationRequest.findById(req.params.id).then((donationRequest) => {
         if (donationRequest) {
             return res.json( JSON.parse(JSON.stringify(donationRequest)) );
         } else {
             return res.status(404).json({error: "The donation request was not found"});
         }
+    }).catch(() => {
+        return res.status(400).json({error: "Invalid donation request ID"});
     });
 });
 
