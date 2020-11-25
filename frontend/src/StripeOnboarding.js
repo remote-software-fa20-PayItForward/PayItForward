50da6a987df89e91cecb758fceab91132e7f8b5c
Matthew Stillman <mstillman9@verizon.net>
2020-11-24 19:30:20 -0500

    remove unnecessary import

--- frontend/src/StripeOnboarding.js
+++ frontend/src/StripeOnboarding.js
@@ -60,13 +60,7 @@ class StripeOnboarding extends Component {
               }
             });
     }
-    componentDidMount(){
-      const script = document.createElement("script");
-      script.src = "https://js.stripe.com/v3/";
-      script.async = true;
-  
-      document.body.appendChild(script);
-    }
+
     render() {
         return (
             <div>
