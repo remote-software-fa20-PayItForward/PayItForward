0c1dfb493225feef9052d4a7e4f90cdc011b156c
jordanz2k <71350122+jordanz2k@users.noreply.github.com>
2020-12-02 04:10:35 +0800

    Add files via upload

--- backend/models/DonationRequest.js
+++ backend/models/DonationRequest.js
@@ -6,6 +6,7 @@ const DonationRequestSchema = new mongoose.Schema({
     description: {type: String, required: true},
     category: {type: String, required: true, enum: ["groceries", "gas", "books"]},
     amount: {type: Number, required: true},
+    amountCollected: {type: Number, required: true, default: 0},
     user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
     subscribers: {type:Array , "default":[]},
     status: {type: String, required: true, enum: ["active", "completed", "cancelled"]}
