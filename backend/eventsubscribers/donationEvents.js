1008c46d34059dad5d3b1d59d9a727029e0b958f
al5361 <57008424+al5361@users.noreply.github.com>
2020-12-02 21:07:55 -0500

    testing needed @jordan

--- backend/eventsubscribers/donationEvents.js
+++ backend/eventsubscribers/donationEvents.js
@@ -1,5 +1,8 @@
 const EventEmitter = require('events');
 const { model } = require('../models/BankItem');
+const User = require( './models/User' );
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 class DonationEventsEmitter extends EventEmitter {}
 
@@ -11,8 +14,32 @@ donationEventsEmitter.on('donationAmountLimitReached', (donationRequest, totalRo
   for(const userSpecificTotalRoundup of totalRoundupByUsers) {
       console.log(`Total roundup of ${userSpecificTotalRoundup.totalRoundup.toFixed(2)} for user ObjectId("${userSpecificTotalRoundup.user_id}")`);
       // TODO: charge the user's CC
-      
-      // TODO: send donationRequest completion email notification to the user
+      const user = User.findById(userSpecificTotalRoundup.user_id).exec();
+      const paymentMethods = await stripe.paymentMethods.list({
+        customer: user.stripeCustomerId,
+        type: 'card',
+      });
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: userSpecificTotalRoundup.totalRoundup.toFixed(2),
+          currency: 'usd',
+          customer: user.stripeCustomerId,
+          payment_method: paymentMethods.id,
+          off_session: true,
+          confirm: true,
+          application_fee_amount: userSpecificTotalRoundup.totalRoundup.toFixed(2) *0.029+30,
+          transfer_data: {
+            destination: donationRequest.user.stripeAccountId,
+        },
+        });
+      } catch (err) {
+        // Error code will be authentication_required if authentication is needed
+        console.log('Error code is: ', err.code);
+        const paymentIntentRetrieved = await stripe.paymentIntents.retrieve(err.raw.payment_intent.id);
+        console.log('PI retrieved: ', paymentIntentRetrieved.id);
+      }
+      // TODO: send donationRequest completion email notification to the user 
+      //enabled in stripe dashboard
   }
 });
 
